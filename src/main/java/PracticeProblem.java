c2f2f75d5d1baf041ea3b2ad2a6e23b04816360f
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-11-20 14:05:36 -0500

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,8 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		Cow cow = new Cow("null", 0, 0);
+		System.out.println(cow.equals(new Object[]{null}));
 	}
 
 }
---
+++ src/main/java/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+    private String name;
+    private int age;
+    private String studentNumber;
+
+    public Student(String name, int age, String studentNumber) {
+        this.name = name;
+        this.age = age;
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getStudentNumber() {
+        return this.studentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.age + " - " + this.studentNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Student)) {
+            return false;
+        }
+        Student student = (Student) obj;
+        return student.getStudentNumber().equals(this.studentNumber);
+    }
+}
