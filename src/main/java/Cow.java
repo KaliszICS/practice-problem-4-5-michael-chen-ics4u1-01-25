bb7b8b536b3c62b2df9cbceb1718760e0bdb0a47
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-11-20 15:15:05 -0500

    javadocs

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,31 +1,62 @@
 public class Cow {
+    /**
+     * Class to describe a cow
+     * @author Michael Chen
+     * @version 1.0.0
+     */
     private String name;
     private int age;
     private double weight;
 
+    /**
+     * Constructor for a cow
+     * @param name Name of the cow
+     * @param age Age of the cow
+     * @param weight Weight of the cow
+     */
     public Cow(String name, int age, double weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
 
+    /**
+     * Gets name of the cow
+     * @return name of the cow
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the age of the cow
+     * @return age of the cow
+     */
     public int getAge() {
         return this.age;
     }
 
+    /**
+     * Gets the weight of the cow
+     * @return weight of the cow
+     */
     public double getWeight() {
         return this.weight;
     }
 
+    /**
+     * Overrides the toString method, returning the name, age, and weight in the format "name, age - weight"
+     * @return cow as a string
+     */
     @Override
     public String toString() {
         return this.name + ", " + this.age + " - " + this.weight;
     }
 
+    /**
+     * Overrides the equals method, comparing instance variables of the Cow class
+     * @return true if instance variables are the same, false if Object is not of type Cow or instance variables are not equal
+     */
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Cow)) {
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,31 +1,62 @@
 public class Student {
+    /**
+     * Class to define a student
+     * @author Michael Chen
+     * @version 1.0.0
+     */
     private String name;
     private int age;
     private String studentNumber;
 
+    /**
+     * Constructor for a student
+     * @param name Name of the student
+     * @param age Age of the student
+     * @param studentNumber Student number of the student
+     */
     public Student(String name, int age, String studentNumber) {
         this.name = name;
         this.age = age;
         this.studentNumber = studentNumber;
     }
 
+    /**
+     * Gets the name of the student
+     * @return name of the student
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the age of the student
+     * @return age of the student
+     */
     public int getAge() {
         return this.age;
     }
 
+    /**
+     * Gets the student number of a student
+     * @return student number of a student
+     */
     public String getStudentNumber() {
         return this.studentNumber;
     }
 
+    /**
+     * Overrides the toString method, returning name, age, and student number in "name, age - student number" format
+     * @return the student as a string
+     */
     @Override
     public String toString() {
         return this.name + ", " + this.age + " - " + this.studentNumber;
     }
 
+    /**
+     * Overrides the equals method, evaluating if the instance variables of the Student is the same
+     * @return true if instance variables are the same, false if Object is not of type Student or instance variables are not equal
+     */
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Student)) {
